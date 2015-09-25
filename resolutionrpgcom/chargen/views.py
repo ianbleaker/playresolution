ee9efbe70a6b6ac20d8f375ca283e07bd24f5d8a
ianbleaker <ianbleaker@gmail.com>
2015-09-25 14:54:18 -0400

    fix untracked

--- resolutionrpgcom/chargen/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-# INDEX #
-# context is the character, send to index.html
-def index(request):
-    return render(request, 'chargen/index.html')
