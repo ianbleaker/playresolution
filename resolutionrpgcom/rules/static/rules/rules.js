bc3ff54a9a2e6a2f173def48e06892bd338f2dc7
ianbleaker <ianbleaker@gmail.com>
2015-09-26 14:19:35 -0400

    basic materialize usage

--- resolutionrpgcom/rules/static/rules/rules.js
+++ resolutionrpgcom/rules/static/rules/rules.js
@@ -1,11 +1,9 @@
 /**
  * Created by ian on 9/24/2015.
  */
-$('#rules-nav').localScroll({
-		target: 'body', // could be a selector or a jQuery object too.
-		duration: 150,
-		offset: -135,
-		hash:false
+$( document).ready(function(){
+	$(".button-collapse").sideNav({
+        edge: 'right'
+    });
+    $('.scrollspy').scrollSpy();
 });
-$('.section-content-ex').addClass('well');
-$('.section-title-ex').html('<i class="mdi-action-subject"></i>').addClass('shadow-z-1');
--- resolutionrpgcom/rules/templatetags/rules_tags.py
+++ resolutionrpgcom/rules/templatetags/rules_tags.py
@@ -22,7 +22,7 @@ def child_nav_list(sections, autoescape=True):
         indent = '\t' * tabs
         shadow_string = ""
         if item.tier() is 1:
-            shadow_string = ' class="shadow-z-1"'
+            shadow_string = ' class="z-depth-1"'
         output.append('%s<li role="presentation"><a href="#%s"%s>%s</a>' % (indent, slugify(item.title), shadow_string, item.title))
         children = sections.filter(parent=item)
         if children.exists():
@@ -34,6 +34,33 @@ def child_nav_list(sections, autoescape=True):
             output.append('%s</ul>' % indent)
             tabs -= 1
         output.append('%s</li>' % indent)
+
+    for section in sections.filter(parent__isnull=True):
+        format_children(section)
+
+    return mark_safe('\n'.join(output))
+
+@register.filter(is_safe=True, needs_autoescape=True)
+def section_text(sections, autoescape=True):
+    if autoescape:
+        escaper = conditional_escape
+    else:
+        escaper = lambda x: x
+
+    output = []
+
+    def format_children(item, tabs=1):
+        indent = '\t' * tabs
+        output.append('%s<div id="%s" class="rule-section section-type-%s scrollspy">' % (indent, slugify(item.title), section.type))
+        tabs += 1
+        output.append('%s<div id="%s-title" class="section-title">%s</div>' % (indent, slugify(item.title), item.title))
+        output.append('%s<div id="%s-content" class="section-content">%s</div>' % (indent, slugify(item.title), item.content))
+        children = sections.filter(parent=item)
+        if children.exists():
+            for child in children:
+                format_children(child)
+        tabs -= 1
+        output.append('%s</div>' % indent)
 
     for section in sections.filter(parent__isnull=True):
         format_children(section)
