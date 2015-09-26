5c8994ad19390080eeaf4b0487d55658b62a323c
ianbleaker <ianbleaker@gmail.com>
2015-09-25 22:35:32 -0400

    css changes

--- resolutionrpgcom/rules/static/rules/rules.js
+++ resolutionrpgcom/rules/static/rules/rules.js
@@ -1,11 +1,11 @@
 /**
  * Created by ian on 9/24/2015.
  */
-
-
 $('#rules-nav').localScroll({
 		target: 'body', // could be a selector or a jQuery object too.
 		duration: 150,
 		offset: -135,
 		hash:false
-	});
+});
+$('.section-content-ex').addClass('well');
+$('.section-title-ex').html('<i class="mdi-action-subject"></i>').addClass('shadow-z-1');
